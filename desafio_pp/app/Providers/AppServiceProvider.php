df2bb657e6e35bc2a91dbfb101d4cf637377edcc
sabinomachado <sabino_machado@hotmail.com>
2023-08-16 11:04:07 -0300

    controller + repo + interface payment

--- desafio_pp/app/Providers/AppServiceProvider.php
+++ desafio_pp/app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use App\Interfaces\PaymentepositoryInterface;
+use App\Repositories\Paymentepository;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -11,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(PaymentRepositoryInterface::class, PaymentRepository::class);
     }
 
     /**
---
+++ desafio_pp/app/Repositories/PaymentRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Payment as Model;
+use App\Interfaces\PaymentRepositoryInterface;
+
+class PaymentRepository implements PaymentRepositoryInterface
+{
+  
+}
